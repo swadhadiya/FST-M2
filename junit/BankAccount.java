fc53bba08285e534ee4b78ca225933f4e9897f51
swadhadiya <107673041+swadhadiya@users.noreply.github.com>
2022-06-24 12:40:47 +0530

    Add files via upload

---
+++ junit/BankAccount.java
@@ -0,0 +1,18 @@
+package exampleClasses;
+
+public class BankAccount {
+
+    private Integer balance;
+
+    public BankAccount(Integer initialBalance) {
+        balance = initialBalance;
+    }
+
+    public Integer withdraw(Integer amount) {
+        if (balance < amount) {
+            throw new NotEnoughFundsException(amount, balance);
+        }
+        balance -= amount;
+        return balance;
+    }
+}
